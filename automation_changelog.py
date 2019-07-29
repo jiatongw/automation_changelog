a24221f41ce0ab7049614e5be01ca8d8d32353a1
hui luo <luoh@vmware.com>
2019-07-28 19:38:57 -0700

    Add a new option to support vversion
    Signed-off-by: Hui Luo <luoh@vmware.com>

--- automation_changelog.py
+++ automation_changelog.py
@@ -3,16 +3,16 @@ import os.path
 import time
 import getopt
 
-deb_template = '''{0} ({1}+vmware.1-1) stable; urgency=optional
+deb_template = '''{0} ({1}+vmware.{8}-1) stable; urgency=optional
 
   * Upstream release: https://github.com/{2}/releases/tag/v{3}
-  * VMware release: https://github.com/heptio/{4}/releases/tag/v{5}+vmware.1
+  * VMware release: https://github.com/heptio/{4}/releases/tag/v{5}+vmware.{9}
 
  -- VMware Release Engineering <{6}>  {7}'''
 
-rpm_template = '''* {0} VMware Release Engineering <{1}>  - {2}-1.vmware.1
+rpm_template = '''* {0} VMware Release Engineering <{1}>  - {2}-1.vmware.{7}
 - Upstream release: https://github.com/{3}/releases/tag/v{4}
-- VMware release: https://github.com/heptio/{5}/releases/tag/v{6}+vmware.1'''
+- VMware release: https://github.com/heptio/{5}/releases/tag/v{6}+vmware.{8}'''
 
 def get_path_and_url(repo):
     debpath = []
@@ -37,13 +37,13 @@ def get_path_and_url(repo):
     return debpath, rpmpath, upstream_url
 
 
-def write_debs(path, major_minor, version, upstream_url, repo, email):
+def write_debs(path, major_minor, version, vversion, upstream_url, repo, email):
     for p in path:
         filename = p + "/changelog-" + major_minor
         s = p.split("/")
         debname = s[-2:-1][0]
         current_time = time.strftime("%a, %d %b %Y %H:%M:%S %z", time.localtime())
-        args = [debname, version, upstream_url, version, repo, version, email, current_time]
+        args = [debname, version, upstream_url, version, repo, version, email, current_time, vversion, vversion]
         if not os.path.isfile(filename):
             with open(filename, "w") as f:
                 f.write(deb_template.format(*args))
@@ -58,10 +58,10 @@ def write_debs(path, major_minor, version, upstream_url, repo, email):
                 n.writelines(lines)
                 n.close()
 
-def write_rpms(path, major_minor, version, upstream_url, repo, email):
+def write_rpms(path, major_minor, version, vversion, upstream_url, repo, email):
     filename = path + "/changelog-" + major_minor
     current_time = time.strftime("%a %b %d %Y", time.localtime())
-    args = [current_time, email, version, upstream_url, version, repo, version]
+    args = [current_time, email, version, upstream_url, version, repo, version, vversion, vversion]
     if not os.path.isfile(filename):
         with open(filename, "w") as f:
             f.write(rpm_template.format(*args))
@@ -76,7 +76,7 @@ def write_rpms(path, major_minor, version, upstream_url, repo, email):
             n.writelines(lines)
             n.close()
 
-def write_version_control_file(isgobuild, major_minor, version):
+def write_version_control_file(isgobuild, major_minor, version, vversion):
     fit_version_control_file = ""
     if isgobuild:
         path = "essentialpks_release/"
@@ -89,14 +89,14 @@ def write_version_control_file(isgobuild, major_minor, version):
     if not fit_version_control_file:
         print("We don't support generating minor release!")
         sys.exit(1)
-    new_version_control_file = path + "essentialpks-v" + version + "+vmware.1"
+    new_version_control_file = path + "essentialpks-v" + version + "+vmware." + vversion
     with open(fit_version_control_file, 'r') as f:
         lines = f.readlines()
         f.close()
     with open(new_version_control_file, 'w') as n:
         for line in lines:
             if "kubernetes" in line:
-                n.writelines("kubernetes=v" + version + "+vmware.1\n")
+                n.writelines("kubernetes=v" + version + "+vmware." + vversion + "\n")
             else:
                 n.writelines(line)
         n.close()
@@ -111,6 +111,7 @@ def usage():
     print('''usage: python3 automation_changelog.py [option] ... [-r | -v | --file ] [arg]
 Options and arguments :
 -v, --version : version; e.g. 1.14.2
+--vversion : vmware version; e.g. 1
 -r, --repo    : kubernetes, etcd, cni-plugins, cri-tools or coredns
 -h, --help    : print help message and exit (also --help)
 -e, --email   : email for changelog
@@ -122,7 +123,7 @@ if __name__ == '__main__':
     isgobuild = False
     generate_version_file = False
     try:
-        options, args = getopt.getopt(sys.argv[1:], "hr:v:ge:", ["help", "repo=", "version=", "file", "email="])
+        options, args = getopt.getopt(sys.argv[1:], "hr:v:ge:", ["help", "repo=", "version=", "vversion=", "file", "email="])
     except getopt.GetoptError:
         print("Please check usage by -h or --help flag")
         usage()
@@ -137,6 +138,9 @@ if __name__ == '__main__':
         if name in ("-v", "--version"):
             version = value
             continue
+        if name in ("--vversion"):
+            vversion = value
+            continue
         if name in ("-g",):
             isgobuild = True
             continue
@@ -150,10 +154,10 @@ if __name__ == '__main__':
     try:
         major_minor = parse_version(version)
         debpath, rpmpath, upstream_url = get_path_and_url(repo)
-        write_debs(debpath, major_minor, version, upstream_url, repo, email)
-        write_rpms(rpmpath, major_minor, version, upstream_url, repo, email)
+        write_debs(debpath, major_minor, version, vversion, upstream_url, repo, email)
+        write_rpms(rpmpath, major_minor, version, vversion, upstream_url, repo, email)
         if generate_version_file:
-            write_version_control_file(isgobuild, major_minor, version)
+            write_version_control_file(isgobuild, major_minor, version, vversion)
     except Exception as e:
         print(e)
         print("Usage: \n")
